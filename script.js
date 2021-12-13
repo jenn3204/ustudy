3b417ea74d416da107944e3366d2259be51f74be
jenniferjaque <jenn3204@stud.kea.dk>
2021-12-13 23:48:22 +0100

    everything is done, might need small changes

--- script.js
+++ script.js
@@ -2,8 +2,47 @@ document.addEventListener("DOMContentLoaded", start);
 
 function start() {
 
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const clas = urlParams.get("class"); 
+
     if (document.querySelector("header")) {
         document.querySelector("#burger_icon").addEventListener("click", burgerMenu); 
+    }
+
+    if (document.querySelector("#classes_dropdown")) {
+        if (window.innerWidth > 800) {
+            document.querySelector("#classes_nav").addEventListener("mouseover", () => {
+                const classesPos = document.querySelector("#classes_nav").getBoundingClientRect(); 
+                const classesLeft = classesPos.left - 30 + "px"; 
+                document.querySelector("#classes_dropdown").classList.remove("hidden"); 
+                document.querySelector("#classes_dropdown").classList.add("dropdown_style"); 
+                document.querySelector("#classes_dropdown").style.left = classesLeft; 
+            })
+
+            document.querySelector("#classes_dropdown").addEventListener("mouseleave", () => {
+                document.querySelector("#classes_dropdown").classList.add("hidden"); 
+                document.querySelector("#classes_dropdown").classList.remove("dropdown_style"); 
+            })
+        }
+    }
+
+    if (document.querySelector("#admin_dropdown")) {
+        if (window.innerWidth > 800) {
+            document.querySelector("#admin_nav").addEventListener("mouseover", () => {
+                const adminPos = document.querySelector("#admin_nav").getBoundingClientRect(); 
+                const adminLeft = adminPos.left - 50 + "px"; 
+                console.log(adminLeft); 
+                document.querySelector("#admin_dropdown").classList.remove("hidden"); 
+                document.querySelector("#admin_dropdown").classList.add("dropdown_style"); 
+                document.querySelector("#admin_dropdown").style.left = adminLeft; 
+            })
+
+            document.querySelector("#admin_dropdown").addEventListener("mouseleave", () => {
+                document.querySelector("#admin_dropdown").classList.add("hidden"); 
+                document.querySelector("#admin_dropdown").classList.remove("dropdown_style"); 
+            })
+        }
     }
     
 
@@ -13,17 +52,18 @@ function start() {
         });
     
         document.querySelector("#log_in_teacher").addEventListener("click", ()  => {
-            location.href = "home.html"
+            location.href = "home_private.html"
         }); 
     }  
 
     if (document.querySelector(".class") ) {
         document.querySelectorAll(".class").forEach(square => {
             square.addEventListener("click", () => {
+                let squareId = square.id; 
                 if (square.classList.contains("private")) {
-                    location.href = "class_private.html"; 
+                    location.href = `class_private.html?class=${squareId}`; 
                 } else {
-                    location.href = "class.html"; 
+                    location.href = `class.html?class=${squareId}`; 
                 }
                 
             })
@@ -32,6 +72,18 @@ function start() {
     }
 
     if (document.querySelector("#class_title")) {
+        let className = clas[0].toUpperCase() + clas.substring(1); 
+        const classTitle = className.replaceAll("_", " "); 
+
+        document.querySelector("#class_title h1").textContent = "Class: " + classTitle; 
+
+        if (window.innerWidth > 800) {
+            document.querySelector(".content").classList.remove("hidden"); 
+
+            document.querySelector("main").style.height = "1250px"; 
+
+        }
+
         document.querySelectorAll(".info_title").forEach(title => {
             title.addEventListener("click", () => {
                 showContent(title); 
@@ -76,6 +128,48 @@ function start() {
         document.querySelector("#ua_btn").addEventListener("click", showUaModal); 
 
     }
+
+    if (document.querySelector("#book_meeting_button")) {
+        document.querySelector("#book_meeting_button").addEventListener("click", () => {
+            if (document.querySelector("#book_meeting_button").classList.contains("private")) {
+                location.href = "book_meeting_private.html"; 
+            } else {
+                location.href = "book_meeting.html"; 
+            }
+        })
+
+        if (window.innerWidth > 800) {
+            document.querySelectorAll(".calendar_content").forEach(cc => {
+                cc.classList.remove("hidden"); 
+        })
+
+    }
+
+        window.addEventListener("resize", () => {
+            console.log("RESIZE"); 
+            if (window.innerWidth > 800) {
+                document.querySelectorAll(".calendar_content").forEach(cc => {
+                    cc.classList.remove("hidden"); 
+                    console.log(cc); 
+                })
+            } else {
+                document.querySelectorAll(".calendar_content").forEach(cc => {
+                    cc.classList.add("hidden"); 
+                })
+            }
+        })
+    }
+
+    if (document.querySelector("#book_meeting")) {
+        document.querySelector("#book_meeting").addEventListener("click", () => {
+            event.preventDefault(); 
+            if (document.querySelector("#book_meeting").classList.contains("private")) {
+                location.href = "calendar_private.html"; 
+            } else {
+                location.href = "calendar.html"; 
+            }
+        })
+    }
     
 }
 
@@ -103,13 +197,16 @@ function burgerMenu() {
 }
 
 function showContent(title) {
-    console.log(title); 
-    console.log(title.nextElementSibling); 
     let titleContent = title.nextElementSibling;
     titleContent.classList.toggle("hidden"); 
-    titleContent.scrollIntoView(); 
 
     document.querySelectorAll(".content").forEach(content => {
+        if (content != titleContent) {
+            content.classList.add("hidden"); 
+        }
+    })
+
+    document.querySelectorAll(".calendar_content").forEach(content => {
         if (content != titleContent) {
             content.classList.add("hidden"); 
         }
